34b665b22b5dcfa25daecbf776a27f4536b90f7c
Erik Hendriks <erik.hendriks@linuxmail.org>
2018-01-13 01:05:29 +0100

    Delete searchinfiles.pl

--- searchinfiles.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-#
-# searches for /pattern/ in files given by argv
-while (<>) {
-      print "$ARGV:$.:$_" if /TODO/;
-  } continue {
-        close ARGV if eof; # Reset $. at the end of each file.
-    }
-
